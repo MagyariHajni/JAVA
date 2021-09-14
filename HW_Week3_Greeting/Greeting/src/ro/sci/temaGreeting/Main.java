a318f2943081db0fc6d5a25043397a36cefd72e6
MagyariHajni <mmhajni@yahoo.com>
2021-09-14 13:49:27 +0300

    HW_Week3_Tema Greeting

---
+++ HW_Week3_Greeting/Greeting/src/ro/sci/temaGreeting/Main.java
@@ -0,0 +1,17 @@
+package ro.sci.temaGreeting;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Who will we be greeting today?");
+        Scanner scanner = new Scanner(System.in);
+        String name= scanner.nextLine();
+
+        if (!name.equals("")) {
+            System.out.println("Hello, "+name);
+        }else {
+            System.out.println("Hello, stranger");
+        }
+    }
+}
