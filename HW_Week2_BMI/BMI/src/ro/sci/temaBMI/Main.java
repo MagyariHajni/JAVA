bba983631f7976f535745d22dd4692bbd0b8bb5c
MagyariHajni <mmhajni@yahoo.com>
2021-09-14 13:13:05 +0300

    Tema Prime

---
+++ HW_Week2_BMI/BMI/src/ro/sci/temaBMI/Main.java
@@ -0,0 +1,73 @@
+package ro.sci.temaBMI;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        boolean goodValue = false;
+        double height = 0;
+        double weight = 0;
+
+        while (!goodValue) {
+            System.out.print("Please enter your height in m: ");
+            height = scanner.nextDouble();
+
+            if (height <= 0) {
+                System.out.println("Please enter a valid height value");
+            } else {
+                goodValue = true;
+            }
+        }
+
+        goodValue = false;
+
+        while (!goodValue) {
+            System.out.print("Please enter your weight in kg: ");
+            weight = scanner.nextDouble();
+
+            if (weight <= 0) {
+                System.out.println("Please enter a valid weight value");
+            } else {
+                goodValue = true;
+            }
+        }
+
+        double bodyMassIndex = weight / height / height;
+        System.out.println("Your BMI is: " + new DecimalFormat("##.##").format(bodyMassIndex));
+
+        String indexcategory = "";
+
+        if (bodyMassIndex <= 18.49) {
+            indexcategory = "underweight";
+        } else {
+            int formattedBmi = (int) (bodyMassIndex - 20) / 5;
+
+            switch (formattedBmi) {
+                case 0:
+                    indexcategory = "normal weight";
+                    break;
+                case 1:
+                    indexcategory = "overweight";
+                    break;
+                case 2:
+                    indexcategory = "class 1 (moderately) obese";
+                    break;
+                case 3:
+                    indexcategory = "class 2 (severely) obese";
+                    break;
+                default:
+                    indexcategory = "class 3 (morbidly) obese";
+            }
+
+        }
+
+        System.out.println("According to the generic BMI table you are: " + indexcategory);
+
+    }
+}
+
+
---
+++ HW_Week2_Prime/Prime/src/ro/sci/temaPrime/Main.java
@@ -0,0 +1,46 @@
+package ro.sci.temaPrime;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        boolean checkNumber = true;
+
+        while (checkNumber) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("What number will we be checking?");
+            int number = scanner.nextInt();
+
+            if (number == 0 || number == 1) {
+                System.out.println(number + " is a special number, but according to the Fundamental Theorem of Arithmetic \n(every integer can be written uniquely as a product of prime numbers)\nit cannot be considered a prime.");
+            } else {
+
+                int numberHalf = number / 2;
+                boolean isPrime = true;
+                int i = 2;
+
+                while (isPrime && i <= numberHalf) {
+                    if (number % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                    i++;
+                }
+
+                String result = (isPrime) ? "a prime" : "not a prime";
+
+                System.out.println(number + " is " + result + " number");
+
+            }
+            System.out.println("************************************************************************");
+            System.out.println("Want to check another number? y/n -n stands for anything else but y :)-");
+            String answer = scanner.next();
+//            checkNumber = (answer.equals("y")) ? true : false;
+            checkNumber = answer.equals("y");
+
+        }
+    }
+}
